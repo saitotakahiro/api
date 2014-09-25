c03aa7e3cefadd9a5245e1ab9d5f5f07ec29f760
saitotakahiro <saito_takahiro@griphone.co.jp>
2014-09-25 18:33:20 +0900

    commit message

--- ci/application/controllers/api.php
+++ ci/application/controllers/api.php
@@ -7,30 +7,104 @@ class Api extends CI_Controller {
 	 */
 	public function musics($id = 0)
 	{
+		mb_http_output('UTF-8');
+
+		$this->load->model('music_model');
 		// メソッドによって判定する
 		if ($_SERVER["REQUEST_METHOD"] != "POST")
 		{
 			// GETの場合は楽曲情報を取得
-			$this->load->model('music_model');
 			$music_data = $this->_get_musics($id);
-			print json_encode($music_data);
+			if ($music_data)
+			{
+				print json_encode($music_data);
+			}
+			else
+			{
+				// 楽曲が存在しない
+                header("HTTP/1.1 404");
+			}
+			exit;
 		}
 		else
 		{
 			// POSTの場合は登録
+			$insert_id = $this->_set_musics();
+			header("Location: http://54.178.124.55/api/musics/{$insert_id}", true, 201);
+			exit;
 		}
+
+	}
+
+
+	/**
+	 * 楽曲
+	 */
+	public function playlists($id = 0)
+	{
+		mb_http_output('UTF-8');
+		$this->load->model('music_model');
+
+			// GETの場合は楽曲情報を取得
+			if ( ! $id)
+			{
+                		header("HTTP/1.1 404");
+               			exit;
+			}
+			$music_data = $this->music_model->get_playlists($id);
+			if ($music_data)
+			{
+				$arr = array(
+					'name'    => $id,
+					'outline' => $music_data[0]['pout'],
+					'musics'  => $music_data,
+				);
+				print json_encode($arr);
+				exit;
+			}
+			else
+			{
+	        	        header("HTTP/1.1 404");
+		                exit;
+			}			
 
 	}
 
 	/**
 	 * 楽曲取得
 	 */
-	private function _get_musics($id)
+	private function _get_musics($name)
 	{
 		// ID指定の場合
-		if ($id > 0)
+		if ( ! is_numeric($name) && $name == 'recent')
 		{
-			$music_data = $this->music_model->get_music_by_id($id);
+			if ($this->input->get('limit'))
+        	        {
+                	        $limit = $this->input->get('limit');
+               		}
+                	else
+                	{
+                	        $limit = 100;
+                	}
+                	if ($this->input->get('start'))
+                	{
+                	        $offset = $this->input->get('start') -1;
+                	}
+                	else
+                	{
+                	        $offset = 0;
+                	}
+                	return $this->music_model->get_music_recent($limit, $offset);
+		}
+		elseif ( ! is_numeric($name) && $name == 'times')
+		{
+			$id = $this->input->get('id');
+			$music_data = $this->music_model->get_music_times($id);
+			return $music_data;	
+		}
+		elseif (is_numeric($name) && $name > 0)
+		{
+			$music_data = $this->music_model->get_music_by_id($name);
 			return $music_data;
 		}
 
@@ -56,5 +130,38 @@ class Api extends CI_Controller {
 		return $this->music_model->get_music($artist_id, $title, $limit, $offset);
 	}
 
+	/**
+	 * 楽曲登録
+	 */
+	private function _set_musics()
+	{
+		// インプット
+		if ($this->input->post('artist_id'))
+		{
+			$artist_id = $this->input->post('artist_id');
+		}
+		else
+		{
+			// 必須パラメータ不足
+			header("HTTP/1.1 400");
+			exit;
+		}
+		if ($this->input->post('title'))
+		{
+			$artist_id = $this->input->post('title');
+		}
+		else
+		{
+			// 必須パラメータ不足
+			header("HTTP/1.1 400");
+			exit;
+		}
+		$outline   = NULL;
+		if ($this->input->post('outline'))
+		{
+			$outline = $this->input->post('outline');
+		}
+		return $this->music_model->set_music($artist_id, $title, $outline);
+	}
 
 }
--- ci/application/models/music_model.php
+++ ci/application/models/music_model.php
@@ -5,7 +5,7 @@ class Music_model extends CI_Model {
     function __construct()
     {
         parent::__construct();
-        $this->load->database();
+	$this->load->database();
     }
 
     /**
@@ -41,5 +41,65 @@ class Music_model extends CI_Model {
 		return $query->result_array();        
     }
 
+    /**
+     * 再生回数
+     */
+    function get_music_times($id)
+    {
+    	if ( ! $id)
+    	{
+    		return;
+    	}
+    	$sql  = "SELECT m.id, count(h.id) as times
+   FROM music as m
+   LEFT JOIN play_history as h ON m.id = h.music_id
+WHERE m.id = {$id}";
+		$query =$this->db->query($sql);
+		
+		return $query->result_array();        
+    }
 
+    /**
+     * 楽曲情報を登録
+     */
+    function set_music($artist_id, $title, $outline)
+    {
+    	// 新規登録
+		$this->db->set('artist_id', $artist_id);
+		$this->db->set('title', $title);
+		$this->db->set('outline', $outline);
+
+		$this->db->insert('music');
+
+		return $this->db->insert_id();
+    }
+
+    /**
+     * プレイリストを取得
+     */
+    function get_playlists($id)
+    {
+    	$sql  = "SELECT m.artist_id, m.id, m.outline, m.title, p.outline as pout
+  FROM playlist as p
+  JOIN playlist_detail as pd ON p.name = pd.playlist_name
+  JOIN music as m ON pd.music_id = m.id
+ WHERE p.name = '{$id}'";
+	$query =$this->db->query($sql);
+		
+	return $query->result_array();        
+    }
+
+    /**
+     * 
+     */
+    function get_music_recent($limit, $offset)
+    {
+	$sql = "SELECT music_id as id, created_at as last_played
+  FROM play_history
+ ORDER BY created_at DESC
+ LIMIT {$offset}, {$limit};";
+        $query =$this->db->query($sql);
+
+        return $query->result_array();
+    }
 }
